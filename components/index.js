20ab5c82dedc44301c05ba5fb9c8c6b2b6c166c0
EuMotta <hyperxmotta@hotmail.com>
2022-12-24 15:28:42 -0300

    exports + imports update

--- components/index.js
+++ components/index.js
@@ -1,20 +1,12 @@
-import Navbar from './Navbar';
 import { TypingText, TitleText } from './CustomTexts';
-import ExploreCard from './ExploreCard';
 import StartSteps from './StartSteps';
 import NewFeatures from './NewFeatures';
 import InsightCard from './InsightCard';
-import Footer from './Footer';
-import Nav from './Nav';
 
 export {
-  Navbar,
-  Nav,
   TitleText,
   TypingText,
   StartSteps,
   NewFeatures,
-  ExploreCard,
   InsightCard,
-  Footer,
 };
